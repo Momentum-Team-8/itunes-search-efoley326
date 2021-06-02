453fb63911ff893c7fa7d704fecf6f56d541247e
Emily Foley <efoley326@gmail.com>
2021-06-02 18:18:00 -0400

    some declarations and functions and my brain is molasses now

---
+++ main.js
@@ -0,0 +1,43 @@
+const url = 'https://proxy-itunes-api.glitch.me/search?term=${artistSearch.value}&limit=15&media=music&entity=song'
+
+const page = document.querySelector('body')
+
+const placeholder = document.querySelector('input')
+
+const url = `https://proxy-itunes-api.glitch.me/search?term=${artistSearch.value}&limit=15&media=music&entity=song`
+
+function retrieveResults(data) {
+    fetch(url)
+    .then(response => response.json)
+    .then(data => renderResults(data))
+    }
+
+page.addEventListener('submit', event => {
+    event.preventDefault()
+    createCards(placeholder.value)
+})
+
+// available attributes: mixTerm, 
+//genreIndex, artistTerm, composerTerm, albumTerm, ratingIndex, songTerm
+
+function renderResults (data) {
+    const searchResults = document.createElement("li")
+    searchResults.id = data.id
+    renderArtistTerm(searchResults, data) 
+    musicInfo.appendChild(searchResults)
+}
+
+function createCards (text, text, _img) {
+    fetch (url, {
+        method: "POST",
+        headers: {"Content-Type" : "application/JSON"} ,
+        body: JSON.stringify({
+            Artist: artistTerm,
+            Song: songTerm,
+            Album: albumTerm
+        })
+        })
+        .then (response => response.json())
+        .then (data => renderResults(data))
+        .then (console.log (banana))
+}
