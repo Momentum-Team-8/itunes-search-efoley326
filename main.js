a9f7ea855be93ddb626033f094f9279a17b9de8e
Emily Foley <efoley326@gmail.com>
2021-06-03 17:57:35 -0400

    I just keep adding things

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 const url = 'https://proxy-itunes-api.glitch.me/search?term=${artistSearch.value}&limit=15&media=music&entity=song'
-
 const page = document.querySelector('body')
-
 const placeholder = document.querySelector('input')
+const play = document.querySelector('.play')
+const pause = document.querySelector('.pause')
+const stop = document.querySelector('.stop')
 
-const url = `https://proxy-itunes-api.glitch.me/search?term=${artistSearch.value}&limit=15&media=music&entity=song`
+//variables ^^^^^^^^^^^^^^^^^^^^^^
+// available attributes: mixTerm, 
+//genreIndex, artistTerm, composerTerm, albumTerm, ratingIndex, songTerm
+//functions vvvvvvvvvvvvvvvvvvvvvv
 
-function createCards (text, text, _img) {
+function retrieveResults() {
+    fetch(url + 'term=' + `${search}`)
+    .then(response => response.json())
+    .then(data => body.innerHTML)
+}
+
+function generateResults () {
     fetch (url, {
         method: "POST",
-        headers: {"Content-Type" : "application/JSON"} ,
         page: JSON.stringify({
             Artist: artistTerm,
             Song: songTerm,
@@ -18,36 +27,64 @@ function createCards (text, text, _img) {
         })
         .then (response => response.json())
         .then (data => renderResults(data))
-        .then (console.log (banana))
+    }
+
+function renderResults () {
+    const searchResults = document.createElement('li')
+    searchResults.id = data.id
+    searchResultsText (searchResults, data)
+    songTitle.appendChild(searchResults)
 }
 
+function searchResultsText (searchResults, data)
+{
+    const controlButtons = document.createElement ('h2')
+    const playButton = document.createElement ('button')
+    playButton.innerHTML = "▶️"
+    playButton.classList.add ("play")
+    const pauseButton = document.createElement ('button')
+    pauseButton.innerHTML = "⏸️ "
+    pauseButton.classList.add ("pause")
+    const stopButton = document.createElement ('button')
+    stopButton.innerHTML = "⏹️"
+    stopButton.classList.add ("stop")
+    controlButtons.appendChild(playButton)
+    controlButtons.appendChild(pauseButton)
+    controlButtons.appendChild(stopButton)
+}
+
+const song = document.createElement('audio')
 
 
 
 
 
-
-
-
-function retrieveResults(data) {
-    fetch(url)
-    .then(response => response.json)
-    .then(data => renderResults(data))
+.play.addEventListener ('click', event => {
+    event.preventDefault();
+    if (event.target.classList.contains("play")) {
+        playSong(event.target)
     }
+})
+.pause.addEventListener ('click', event => {
+    event.preventDefault();
+    if (event.target.classList.contains("pause")) {
+        pauseSong(event.target)
+    }
+})
+playButton.addEventListener ('click', event => {
+    event.preventDefault();
+    if (event.target.classList.contains("stop")) {
+        stopSong(event.target)
+    }
+})
+
+
+
 
 page.addEventListener('submit', event => {
     event.preventDefault()
-    createCards(placeholder.value)
+    renderResults(placeholder.value)
 })
-
-// available attributes: mixTerm, 
-//genreIndex, artistTerm, composerTerm, albumTerm, ratingIndex, songTerm
-
-function renderResults (data) {
-    const searchResults = document.createElement("li")
-    searchResults.id = data.id
-    renderArtistTerm(searchResults, data) 
-    musicInfo.appendChild(searchResults)
-}
+    
 
 
